caff0fe0bc8d0bc90e5204115fcd458a88c655ea
JohnJayMiller <JohnJayMiller@protonmail.com>
2022-04-02 17:24:19 -0500

    quick edit

--- chatBot.js
+++ chatBot.js
@@ -1,7 +1,7 @@
 function chat(){
     var inputVal = document.getElementById("usermsg").value;
     document.getElementById("chatbox").innerHTML += "<br>" +"User:  "  + inputVal + "<br>";
-    if (inputVal.includes("slow") && inputVal.includes("laptop")) {
+    if (inputVal.includes("laptop")) {
         document.getElementById("chatbox").innerHTML += "<br>" +"Chatbot:  "  + "Try 5 Common Laptop Problems and How to Repair Them: https://greatlakescomputer.com/blog/5-common-laptop-problems-and-how-to-repair-them" + "<br>";
 
     }
