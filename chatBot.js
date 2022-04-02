04ed06566d20f074006fe5d9f802eb5c1389b082
JohnJayMiller <JohnJayMiller@protonmail.com>
2022-04-02 12:58:59 -0500

    added another line break

--- chatBot.js
+++ chatBot.js
@@ -1,8 +1,8 @@
 function chat(){
     var inputVal = document.getElementById("usermsg").value;
-    document.getElementById("chatbox").innerHTML += "<br>" +"User:  "  + inputVal;
+    document.getElementById("chatbox").innerHTML += "<br>" +"User:  "  + inputVal + "<br>";
     if (inputVal.includes("slow") && inputVal.includes("laptop")) {
-        document.getElementById("chatbox").innerHTML += "<br>" +"Chatbot:  "  + "Try 5 Common Laptop Problems and How to Repair Them: https://greatlakescomputer.com/blog/5-common-laptop-problems-and-how-to-repair-them";
+        document.getElementById("chatbox").innerHTML += "<br>" +"Chatbot:  "  + "Try 5 Common Laptop Problems and How to Repair Them: https://greatlakescomputer.com/blog/5-common-laptop-problems-and-how-to-repair-them" + "<br>";
 
     }
 }
