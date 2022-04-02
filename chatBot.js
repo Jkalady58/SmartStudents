e8eacb119a72a4e65615c70e9e08a125938fcf01
JohnJayMiller <JohnJayMiller@protonmail.com>
2022-04-02 12:54:49 -0500

    cheat reply function added

--- chatBot.js
+++ chatBot.js
@@ -1,4 +1,8 @@
 function chat(){
     var inputVal = document.getElementById("usermsg").value;
     document.getElementById("chatbox").innerHTML += "<br>" +"User:  "  + inputVal;
+    if (inputVal.includes("slow") && inputVal.includes("laptop")) {
+        document.getElementById("chatbox").innerHTML += "<br>" +"Chatbot:  "  + "Try 5 Common Laptop Problems and How to Repair Them: https://greatlakescomputer.com/blog/5-common-laptop-problems-and-how-to-repair-them";
+
+    }
 }
