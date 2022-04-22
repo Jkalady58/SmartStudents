e702a6684e46e4f5b20111b49283ebd8c887b5fc
JohnJayMiller <JohnJayMiller@protonmail.com>
2022-04-22 14:36:25 -0500

    moved chatbox and bot behavior to js file

--- chatBot.js
+++ chatBot.js
@@ -1,13 +1,20 @@
-function chat(){
-    var inputVal = document.getElementById("usermsg").value;
+ function openForm() {
+      document.getElementById("myForm").style.display = "block";
+    }
+
+    function closeForm() {
+      document.getElementById("myForm").style.display = "none";
+    }
+    function chat(){
+        var inputVal = document.getElementById("usermsg").value;
     var yesbutton = document.createElement('button');yesbutton.type = 'button';yesbutton.innerHTML = 'Yes';
     var nobutton = document.createElement('button');nobutton.type = 'button';nobutton.innerHTML = 'No';
     nobutton.onclick = function() {
-        document.getElementById("chatbox").innerHTML += "<br>" + "Would you like to generate a ticket?";
+        document.getElementById("chatbox").innerHTML += "<br>" + "Would you like to generate a ticket?" + "<br";
     }
     document.getElementById("chatbox").innerHTML += "<br>" +"User:  "  + inputVal + "<br>";
-    document.getElementById("chatbox").innerHTML += "<br>" +"Chatbot: Generating results for "  + inputVal + "<br>" +"Chatbot: Did that answer your question?" + "<br>";
+    document.getElementById("chatbox").innerHTML += "Chatbot: Generating results for "  + inputVal + "<br>" +"Chatbot: Did that answer your question?" + "<br>";
     //print out results from database here
     document.getElementById("chatbox").appendChild(yesbutton);
     document.getElementById("chatbox").appendChild(nobutton);
-}
+    }
